72565fb54050a988379143ebc0e1a7c356901ef1
gaddiat <gaddiat@mail.uc.edu>
2024-02-08 02:25:26 +0000

    revised email.js to ensure validation

--- labs/lab2/email.js
+++ labs/lab2/email.js
@@ -1,17 +1,21 @@
 var shown = false;
-function showhideEmail(){
-	if (shown){
-		document.getElementById('email').innerHTML = "show my email";
-		shown = false;
-	}else{
 
-		var myemail = "<a href='mailto:gaddiat"+ "@" +
+function showhideEmail() {
+    if (shown) {
+        document.getElementById('email').innerHTML = "show my email";
+        shown = false;
+    } else {
+        var emailElement = document.getElementById('email');
+        var myemail = "gaddiat@ucmail.uc.edu"; // Default email value
+        
+        // Validate the email data
+        if (typeof myemail !== "undefined" && myemail !== null && myemail.trim() !== "") {
+            myemail = "<a href='mailto:" + myemail + "'>" + myemail + "</a>";
+        } else {
+            myemail = "Email data is not available";
+        }
 
-"ucmail.uc.edu'>gaddiat" + "@" + "ucmail.uc.edu</a>";
-
-document.getElementById('email').innerHTML= myemail;
-
-shown= true;
-	}
-
+        emailElement.innerHTML = myemail;
+        shown = true;
+    }
 }
