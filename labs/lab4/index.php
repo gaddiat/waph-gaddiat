62a4a22ae672f8627eb2c1c83fa3ac1b0e437fe3
gaddiat <gaddiat@mail.uc.edu>
2024-03-05 03:15:52 +0000

    updated V1.1

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,13 +1,42 @@
 <?php
-	session_start();    
-	if (checklogin_mysql($_POST["username"],$_POST["password"])) {
-?>
-	<h2> Welcome <?php echo htmlentities($_POST['username']); ?> !</h2>
-<?php		
-	}else{
-		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
+	
+	//session_set_cookie_params(15*60,"/","192.168.33.128"TRUE,TRUE);
+	session_set_cookie_params([
+	    'lifetime' => 15*60,
+	    'path' => '/',
+	    'domain' => 'gaddiat.waph.io',
+	    'secure' => TRUE,
+	    'httponly' => TRUE
+	]);
+
+	session_start();  
+
+	if (isset($_POST["username"]) and isset($_POST["password"])){
+		if (checklogin_mysql($_POST["username"],$_POST["password"])) {
+			$_SESSION['authenticated'] = TRUE;
+			$_SESSION['username'] = $_POST["username"];
+			$_SESSION['browser'] = $_SERVER["HTTP_USER_AGENT"];
+		}else{
+			session_destroy();
+			echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
+			die();
+		}
+	}
+	if (!isset($_SESSION['authenticated']) or $_SESSION['authenticated'] != TRUE) {
+		session_destroy();
+		echo "<script>alert('You have not loggedin,please login first!')</script>";
+		header("Refresh: 0; url=form.php");
 		die();
 	}
+
+	if ($_SESSION['browser'] != $_SERVER["HTTP_USER_AGENT"]) {
+    session_destroy();
+    echo "<script>alert('Session hijacking is detected')</script>";
+    header("Refresh: 0; url=form.php");
+    die();
+}
+
+
 	function checklogin_mysql($username, $password) {
 		$mysqli = new mysqli('localhost','gaddiat','1234','waph');
 		if($mysqli->connect_errno){
@@ -23,4 +52,7 @@
 			return TRUE;
 		return FALSE;
   	}
+
 ?>
+		<h2> Welcome <?php echo htmlentities($_SESSION['username']); ?> !</h2>
+		<a href="logout.php">logout</a>
---
+++ labs/lab4/logout.php
@@ -0,0 +1,6 @@
+<?php
+	session_start();
+	session_destroy();
+?>
+<p> You have logged out!</p>
+<a href="form.php">Login again</a>
